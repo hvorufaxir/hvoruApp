20a3267eec2ec337e8a02114b46fcf1073cc3dde
hvorufaxir <152877428+hvorufaxir@users.noreply.github.com>
2025-07-11 18:56:28 -0600

    Create account-overview-unknown.stories.tsx

---
+++ ui/components/multichain/account-overview/account-overview-unknown.stories.tsx
@@ -0,0 +1,9 @@
+import AccountOverviewUnknown from './AccountOverviewUnknown';
+import { AccountOverviewCommonProps } from './common';
+
+export default {
+  title: 'Components/Multichain/AccountOverviewUnknown',
+component: AccountOverviewUnknown,
+}
+
+export const DefaultStory = (props) => <AccountOverviewCommonProps {...props} />;
